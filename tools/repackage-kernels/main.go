c31b3ecdc2e44cfe195f6ea001f61b39f037d132
Josh Komoroske <josh@stackrox.com>
2019-02-11 15:27:11 -0800

    Generate and download package list for building

--- tools/repackage-kernels/main.go
+++ tools/repackage-kernels/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 
 	"github.com/fatih/color"
@@ -32,16 +33,21 @@ func mainCmd() error {
 	var (
 		flagManifest = flag.String("manifest", "", "Path to build manifest file.")
 		flagCacheDir = flag.String("cache-dir", "", "Path to build cache directory.")
-		flagAction   = flag.String("action", "build", `Action to take. (one of "build" or "combine")`)
+		flagAction   = flag.String("action", "build", `Action to take. (one of "build", "combine", or "files")`)
+		flagPrefix   = flag.String("prefix", "", "Prefix to prepend to file list.")
+		flagPkgDir   = flag.String("pkg-dir", "", "Path to downloaded package dir.")
 	)
 	flag.Parse()
 
 	switch *flagAction {
 	case "build":
-		return buildCmd(*flagManifest, *flagCacheDir)
+		return buildCmd(*flagManifest, *flagCacheDir, *flagPkgDir)
 
 	case "combine":
 		return combineCmd(*flagCacheDir)
+
+	case "files":
+		return filesCmd(*flagManifest, *flagCacheDir, *flagPrefix)
 
 	default:
 		return errors.New("unknown action")
@@ -51,7 +57,7 @@ func mainCmd() error {
 // buildCmd is the action that is run when the flag -action=build is used.
 // This action will build all possible manifests, except for the ones that
 // already exist in the build cache, or fall on a different CircleCI build node.
-func buildCmd(manifestFile string, cacheDir string) error {
+func buildCmd(manifestFile string, cacheDir string, pkgDir string) error {
 	var (
 		cacheFile = filepath.Join(cacheDir, "cache.yml")
 		count     int
@@ -87,7 +93,7 @@ func buildCmd(manifestFile string, cacheDir string) error {
 
 		var (
 			builder = buildManifest[id]
-			err     = build(builder, id)
+			err     = build(builder, id, pkgDir)
 		)
 
 		// This build failed. Report it and move along.
@@ -126,8 +132,69 @@ func combineCmd(cacheDir string) error {
 	return errors.Wrap(err, "failed to save combined cache")
 }
 
+// filesCmd is the action that is run when the flag -action=files is used.
+// This action combines a list of GCS bucket objects that need to be downloaded
+// for a subsequent build.
+func filesCmd(manifestFile string, cacheDir string, prefix string) error {
+	var (
+		cacheFile = filepath.Join(cacheDir, "cache.yml")
+		filesSet  = make(map[string]struct{})
+		count     int
+	)
+
+	// buildCache is a record of all builds, that were successfully built.
+	buildCache, err := manifest.Load(cacheFile)
+	if err != nil {
+		return errors.Wrap(err, "failed to load build cache")
+	}
+
+	// buildManifest is a record of all possible builds.
+	buildManifest, err := manifest.Load(manifestFile)
+	if err != nil {
+		return errors.Wrap(err, "failed to load build manifest")
+	}
+
+	for _, id := range buildManifest.SortedIDs() {
+		// Skip this build if it already exists in the cache.
+		if _, found := buildCache[id]; found {
+			continue
+		}
+
+		// Skip this build if it does not fall on this (CircleCI) build node.
+		if count%nodeCount != nodeIndex {
+			count++
+			continue
+		} else {
+			count++
+		}
+
+		var builder = buildManifest[id]
+
+		// Add every package in this builder to the combined package set.
+		for _, pkg := range builder.Packages {
+			filesSet[pkg] = struct{}{}
+		}
+	}
+
+	// Print out all the packages in alphabetical order, with the given prefix
+	// prepended to the front.
+	for _, pkg := range sortedSet(filesSet) {
+		fmt.Printf("%s/%s\n", prefix, pkg)
+	}
+
+	return nil
+}
+
 // build runs a repackage build for the given manifest.
-func build(builder manifest.Builder, id string) error {
+func build(builder manifest.Builder, id string, pkgDir string) error {
+	// Check if all packages exist locally. Fail build if any of them do not.
+	for _, pkg := range builder.Packages {
+		filename := filepath.Join(pkgDir, pkg)
+		if !exists(filename) {
+			return errors.Errorf("package file %s does not exist", filename)
+		}
+	}
+
 	return errors.New("not yet implemented")
 }
 
@@ -158,4 +225,26 @@ func envInt(key string, fallback int) int {
 		return fallback
 	}
 	return number
+}
+
+// exists checks if the given file exists on disk.
+func exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+// sortedSet returns the keys for the given set, in alphabetical order.
+func sortedSet(set map[string]struct{}) []string {
+	var list = make([]string, 0, len(set))
+
+	for item := range set {
+		list = append(list, item)
+	}
+
+	sort.Strings(list)
+	return list
 }
