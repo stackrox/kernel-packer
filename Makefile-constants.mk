37e9868cb1d17e0218e88a83072aa148c9f210d5
Josh Komoroske <josh@stackrox.com>
2019-02-19 15:32:21 -0800

    Generate and commit build manifest during crawl

---
+++ Makefile-constants.mk
@@ -0,0 +1,10 @@
+# Repository root directory. Must be set.
+ifndef ROOT_DIR
+$(error ROOT_DIR is not set)
+endif
+
+# Directory to output crawled text files into.
+CRAWLED_PACKAGE_DIR = $(ROOT_DIR)/kernel-package-lists
+
+# GCS bucket for storing kernel header packages
+KERNEL_PACKAGE_BUCKET = gs://stackrox-kernel-packages
