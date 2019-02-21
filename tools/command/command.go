90dfe1ec5bdd64457e2ff39769ff24ee94ebeadd
Josh Komoroske <josh@stackrox.com>
2019-02-21 13:43:43 -0800

    Repackage RedHat kernels

---
+++ tools/command/command.go
@@ -0,0 +1,70 @@
+package command
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+// Run will exec the given command and stream all output (stdout and stderr)
+// back to the current terminal.
+func Run(name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	go io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stderr, stderr)
+
+	return cmd.Run()
+}
+
+func DockerCommand(distroName string, outputDir string, packages []string) (string, []string, error) {
+	var cmd = "docker"
+	var args = []string{
+		"run",
+		"--rm",
+		"-t",
+	}
+
+	if len(packages) == 0 {
+		return "", nil, errors.New("no packages given")
+	}
+
+	if !filepath.IsAbs(outputDir) {
+		return "", nil, errors.New("output directory is not an absolute path")
+	}
+
+	// Add a series of read-only volume mappings for each of the given packages.
+	for index, pkg := range packages {
+		if !filepath.IsAbs(pkg) {
+			return "", nil, errors.New("package is not an absolute path")
+		}
+		var volumeMapping = fmt.Sprintf("%s:/input/package-%d:ro", pkg, index)
+		args = append(args, "-v", volumeMapping)
+	}
+
+	// Add a single volume mapping for the output directory.
+	args = append(args, "-v", fmt.Sprintf("%s:/output", outputDir))
+
+	// Add the Docker image name, distro name, and output directory alias
+	args = append(args, "repackage:latest", distroName, "/output")
+
+	// Add a series of package names, same as the volume aliases.
+	for index := range packages {
+		args = append(args, fmt.Sprintf("/input/package-%d", index))
+	}
+
+	return cmd, args, nil
+}
---
+++ tools/command/command_test.go
@@ -0,0 +1,94 @@
+package command
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test(t *testing.T) {
+	tests := []struct {
+		title        string
+		distro       string
+		outputDir    string
+		packages     []string
+		err          string
+		expectedArgs []string
+	}{
+		{
+			title: "empty packages",
+			err:   "no packages given",
+		},
+		{
+			title:     "relative output dir",
+			outputDir: ".build-data/bundles",
+			packages:  []string{"fake"},
+			err:       "output directory is not an absolute path",
+		},
+		{
+			title:     "relative package",
+			outputDir: "/.build-data/bundles",
+			packages:  []string{"package.rpm"},
+			err:       "package is not an absolute path",
+		},
+		{
+			title:     "one relative package",
+			outputDir: "/.build-data/bundles",
+			packages:  []string{"/package-a.rpm", "package-b.rpm", "/package-c.rpm"},
+			err:       "package is not an absolute path",
+		},
+		{
+			title:     "single package",
+			distro:    "RedHat",
+			outputDir: "/.build-data/bundles",
+			packages:  []string{"/package.rpm"},
+			expectedArgs: []string{
+				"run", "--rm", "-t",
+				"-v", "/package.rpm:/input/package-0:ro",
+				"-v", "/.build-data/bundles:/output",
+				"repackage:latest", "RedHat", "/output", "/input/package-0",
+			},
+		},
+		{
+			title:     "multiple package",
+			distro:    "RedHat",
+			outputDir: "/.build-data/bundles",
+			packages: []string{
+				"/package-a.rpm",
+				"/dir/package-b.rpm",
+				"/package-c.rpm",
+			},
+			expectedArgs: []string{
+				"run", "--rm", "-t",
+				"-v", "/package-a.rpm:/input/package-0:ro",
+				"-v", "/dir/package-b.rpm:/input/package-1:ro",
+				"-v", "/package-c.rpm:/input/package-2:ro",
+				"-v", "/.build-data/bundles:/output",
+				"repackage:latest", "RedHat", "/output",
+				"/input/package-0",
+				"/input/package-1",
+				"/input/package-2",
+			},
+		},
+	}
+
+	for index, test := range tests {
+		name := fmt.Sprintf("%d %s", index+1, test.title)
+		t.Run(name, func(t *testing.T) {
+
+			actualCmd, actualArgs, actualErr := DockerCommand(test.distro, test.outputDir, test.packages)
+
+			if test.err != "" {
+				require.EqualError(t, actualErr, test.err)
+				return
+			} else {
+				require.NoError(t, actualErr)
+			}
+
+			require.Equal(t, "docker", actualCmd)
+
+			require.Equal(t, test.expectedArgs, actualArgs)
+		})
+	}
+}
