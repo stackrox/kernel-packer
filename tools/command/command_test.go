7464c243e8d42723684a498833587bf1c30b1811
Robby Cochran <rc@stackrox.com>
2019-03-15 00:26:34 +0000

    Prepare gcos kernel config before packing

--- tools/command/command_test.go
+++ tools/command/command_test.go
@@ -47,9 +47,9 @@ func Test(t *testing.T) {
 			packages:  []string{"/package.rpm"},
 			expectedArgs: []string{
 				"run", "--privileged", "--rm", "-t",
-				"-v", "/package.rpm:/input/package-0:ro",
+				"-v", "/:/input:ro",
 				"-v", "/.build-data/bundles:/output",
-				"repackage:latest", "sha", "redhat", "/output", "/input/package-0",
+				"repackage:latest", "sha", "redhat", "/output", "/input/package.rpm",
 			},
 		},
 		{
@@ -59,19 +59,17 @@ func Test(t *testing.T) {
 			outputDir: "/.build-data/bundles",
 			packages: []string{
 				"/package-a.rpm",
-				"/dir/package-b.rpm",
+				"/package-b.rpm",
 				"/package-c.rpm",
 			},
 			expectedArgs: []string{
 				"run", "--privileged", "--rm", "-t",
-				"-v", "/package-a.rpm:/input/package-0:ro",
-				"-v", "/dir/package-b.rpm:/input/package-1:ro",
-				"-v", "/package-c.rpm:/input/package-2:ro",
+				"-v", "/:/input:ro",
 				"-v", "/.build-data/bundles:/output",
 				"repackage:latest", "sha", "redhat", "/output",
-				"/input/package-0",
-				"/input/package-1",
-				"/input/package-2",
+				"/input/package-a.rpm",
+				"/input/package-b.rpm",
+				"/input/package-c.rpm",
 			},
 		},
 	}
