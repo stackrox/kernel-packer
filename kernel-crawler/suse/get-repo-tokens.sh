2e15cd70ecc7a3c2b4960ab1f7ddbc925da332b6
Robby Cochran <rc@stackrox.com>
2021-01-27 14:48:42 -0800

    docs

--- kernel-crawler/suse/get-repo-tokens.sh
+++ kernel-crawler/suse/get-repo-tokens.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Generate a JSON containing SUSE repositories and the access token for each.
+# The output is an array of JSON objects with the keys, 'name','url', and 'token'
+# Each repository URL has a unique token and is used as a query parameter.
+#
+# See the RMT source code for more details:
+# https://github.com/SUSE/rmt/blob/master/lib/suse/connect/api.rb#L108
+
 SUSE_REPO_URL="https://scc.suse.com/connect/organizations/repositories"
 
 main() {
@@ -13,7 +20,8 @@ main() {
     # Get each repo page in json
     seq "${last_page}" | xargs -I{} -P4 -- curl -o "${tmp_dir}/repo_{}" -s -u "${userpass}" "${SUSE_REPO_URL}?page={}"
 
-    # Join results, select x86_64 repos, and split out authorization token from the URL. Each url is of the form https://{URL}?{TOKEN}
+    # Join results, filter just x86_64 repos, and split out authorization token from the URL.
+    # Each url is of the form https://{URL}?{TOKEN}
     cat "${tmp_dir}"/repo_* | jq -s '[.[]] | flatten' | \
         jq '[.[] | select(.distro_target != null) | select(.distro_target | contains("x86_64"))]' | \
         jq 'map(.token = (.url | split("?")[1])) | map(.url |= split("?")[0]) | map({name,url,token})'
