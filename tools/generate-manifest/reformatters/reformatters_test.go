868adbd41f587bf78904f739001eef09c5c80e71
Robby Cochran <rc@stackrox.com>
2021-08-04 19:56:40 -0700

    reformatters test update

--- tools/generate-manifest/reformatters/reformatters_test.go
+++ tools/generate-manifest/reformatters/reformatters_test.go
@@ -72,6 +72,26 @@ func TestReformatPairs(t *testing.T) {
 			},
 		},
 		{
+			title: "backport debs remain if 16.04",
+			packages: []string{
+				"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-gke-headers-5.4.0-1048_5.4.0-1048.50_amd64.deb",
+				"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-headers-5.4.0-1048-gke_5.4.0-1048.50_amd64.deb",
+				"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-gke-headers-5.4.0-1048_5.4.0-1048.50~16.04.1_amd64.deb",
+				"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-headers-5.4.0-1048-gke_5.4.0-1048.50~16.04.1_amd64.deb",
+			},
+			manifests: [][]string{
+				{
+					"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-gke-headers-5.4.0-1048_5.4.0-1048.50_amd64.deb",
+					"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-headers-5.4.0-1048-gke_5.4.0-1048.50_amd64.deb",
+				},
+				{
+					"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-gke-headers-5.5.0-786_5.5.0-786.50~18.04.1_amd64.deb",
+					"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-headers-5.5.0-786-gke_5.5.0-786.50~18.04.1_amd64.deb",
+				},
+			},
+		},
+
+		{
 			title: "regulars trump backport debs when after",
 			packages: []string{
 				"http://security.ubuntu.com/ubuntu/pool/main/l/linux-gke/linux-gke-headers-5.4.0-1048_5.4.0-1048.50~18.04.1_amd64.deb",
@@ -92,7 +112,7 @@ func TestReformatPairs(t *testing.T) {
 		name := fmt.Sprintf("%d %s", index+1, test.title)
 		t.Run(name, func(t *testing.T) {
 			actual, _ := reformatPairs(test.packages)
-			assert.Equal(t, test.manifests, actual)
+			assert.ElementsMatch(t, test.manifests, actual)
 		})
 	}
 }
