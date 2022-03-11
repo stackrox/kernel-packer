4701ba3f0fb32529a1f46cc890acbc4207b7af29
JoukoVirtanen <jouko@stackrox.com>
2022-03-11 11:04:11 -0800

    Changed logLevel to debug

--- kernel-crawler/rhel-subscription-crawler/crawl.sh
+++ kernel-crawler/rhel-subscription-crawler/crawl.sh
@@ -4,4 +4,4 @@ set -eou pipefail
 rhel_package_lists=/tmp/rhel_package_lists.txt
 ls /kernel-package-lists/rhel[0-9]*.txt > "$rhel_package_lists"
 
-python crawler.py --rhelPackageLists "$rhel_package_lists" --logLevel DEBUG 
+python crawler.py --rhelPackageLists "$rhel_package_lists"
