a5e57f43bc3c4e97b7189f4165fce9618a99e05c
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-05-27 14:54:45 +0200

    Prepare scripts for google-cloud-sdk
    There seems to be no standartized way of installing google-cloud-sdk,
    simulare gcloud-orb explicitely via scripts.

---
+++ .openshift-ci/google-cloud-sdk/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+GOOGLE_CLOUD_SDK_VERSION=383.0.0
+
+install () {
+  # Set sudo to work whether logged in as root user or non-root user
+  if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; w
+  curl -Ss --retry 5 https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-"${GOOGLE_CLOUD_SDK_VERSION}"-linux-x86_64.tar.gz | tar xz
+  echo 'source ~/google-cloud-sdk/path.bash.inc' >> $BASH_ENV
+}
+
+if [[ $(command -v gcloud) == "" ]]; then
+    install
+else
+    echo "gcloud CLI is already installed."
+w
