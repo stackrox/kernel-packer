ce95482439eccdb693ad86d4fae7243adbaf06b9
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-06-02 14:48:02 +0200

    Add main crawl script
    For now do crawling in staging mode unconditionaly, to simplify testing.

---
+++ .openshift-ci/crawl/main.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Assume we need to run in staging mode unconditionally for testing purposes.
+
+# The below variables ontain a comma delimited list of GCP buckets,
+# Scripts may read from all buckets but only write to the first bucket in the list.
+KERNEL_PACKAGE_STAGING_BUCKET="gs://stackrox-kernel-packages-staging/${CIRCLE_BRANCH}/${CIRCLE_SHA1}"
+KERNEL_BUNDLE_STAGING_BUCKET="gs://stackrox-kernel-bundles-staging/${CIRCLE_BRANCH}/${CIRCLE_SHA1}"
+KERNEL_PACKAGE_BUCKET="${KERNEL_PACKAGE_STAGING_BUCKET},gs://stackrox-kernel-packages"
+KERNEL_BUNDLE_BUCKET="${KERNEL_BUNDLE_STAGING_BUCKET},gs://stackrox-kernel-bundles"
+
+echo "KERNEL_BUNDLE_STAGING_BUCKET=${KERNEL_BUNDLE_STAGING_BUCKET}"
+echo "KERNEL_PACKAGE_STAGING_BUCKET=${KERNEL_PACKAGE_STAGING_BUCKET}"
+echo "KERNEL_BUNDLE_BUCKET=${KERNEL_BUNDLE_BUCKET}"
+echo "KERNEL_PACKAGE_BUCKET=${KERNEL_PACKAGE_BUCKET}"
+
+cat >>"$BASH_ENV" \<<EOF
+export KERNEL_PACKAGE_STAGING_BUCKET="${KERNEL_PACKAGE_STAGING_BUCKET}"
+export KERNEL_BUNDLE_STAGING_BUCKET="${KERNEL_BUNDLE_STAGING_BUCKET}"
+export KERNEL_PACKAGE_BUCKET="${KERNEL_PACKAGE_BUCKET}"
+export KERNEL_BUNDLE_BUCKET="${KERNEL_BUNDLE_BUCKET}"
+EOF
+
+if ! make -j -k crawl 2> >(tee /tmp/make-crawl-stderr >&2) ; then
+    touch /tmp/crawl-failed
+fi
+
+./scripts/restore-removed
