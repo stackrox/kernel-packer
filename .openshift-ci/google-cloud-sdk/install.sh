2bcafdd9cc33ffbdc0731890f2883c7f4da1394e
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-06-08 11:57:42 +0200

    Correct path for sourcing

--- .openshift-ci/google-cloud-sdk/install.sh
+++ .openshift-ci/google-cloud-sdk/install.sh
@@ -10,7 +10,7 @@ install () {
   # Set sudo to work whether logged in as root user or non-root user
   if [[ $EUID == 0 ]]; then export SUDO=""; else export SUDO="sudo"; fi
   curl -Ss --retry 5 https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-"${GOOGLE_CLOUD_SDK_VERSION}"-linux-x86_64.tar.gz | tar xz
-  source ~/google-cloud-sdk/path.bash.inc
+  source ./google-cloud-sdk/path.bash.inc
 }
 
 if [[ $(command -v gcloud) == "" ]]; then
