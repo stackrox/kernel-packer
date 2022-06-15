9e01e24732127a7479133b43d586dae82ce67e7c
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-06-15 16:53:29 +0200

    Use correct path

--- kernel-crawler/rhel-subscription-crawler/crawl.sh
+++ kernel-crawler/rhel-subscription-crawler/crawl.sh
@@ -3,6 +3,6 @@ set -eou pipefail
 
 rhel_package_lists=/tmp/rhel_package_lists.txt
 #ls /kernel-package-lists/rhel[0-9]*.txt > "$rhel_package_lists"
-ls /kernel-package-lists/rhel.txt > "$rhel_package_lists"
+ls $(CRAWLED_PACKAGE_DIR)/rhel.txt > "$rhel_package_lists"
 
 python crawler.py --all=true --rhelPackageLists "$rhel_package_lists"
