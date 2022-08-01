66bd3ce36f2df430a8102a1e145575bd6bd38a65
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-08-01 13:49:54 +0200

    Indentation

--- .openshift-ci/crawler/cache.sh
+++ .openshift-ci/crawler/cache.sh
@@ -19,9 +19,9 @@ make clean-cache
 echo "Sanity check bundles..."
 IFS=',' read -r -a bucket_names <<< "${KERNEL_BUNDLE_BUCKET}"
 for bucket_name in "${bucket_names[@]}"; do
-echo "Kernel versions in ${bucket_name}"
-gsutil ls "${bucket_name}/**.tgz" | \
-  sed 's|^.*bundle-||' | sed 's|\.tgz$||' | sort || true
+  echo "Kernel versions in ${bucket_name}"
+  gsutil ls "${bucket_name}/**.tgz" | \
+    sed 's|^.*bundle-||' | sed 's|\.tgz$||' | sort || true
 done
 
 gsutil cp .build-data/cache/cache.yml "${KERNEL_BUNDLE_BUCKET}/cache.yml"
