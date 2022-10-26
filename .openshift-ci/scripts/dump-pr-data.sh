7ffe9588addafc1b45b3ef815b32e84f116d4ee8
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-10-26 14:52:04 +0200

    Minor fix

--- .openshift-ci/scripts/dump-pr-data.sh
+++ .openshift-ci/scripts/dump-pr-data.sh
@@ -4,25 +4,25 @@ set -exo pipefail
 touch /tmp/ci-data.sh
 
 if [[ -n "${REPO_OWNER}" ]]; then
-    echo "export REPO_OWNER='${REPO_OWNER}'" >> /tmp/ci-data/dump.sh
+    echo "export REPO_OWNER='${REPO_OWNER}'" >> /tmp/ci-data.sh
 fi
 
 if [[ -n "${REPO_NAME}" ]]; then
-    echo "export REPO_NAME='${REPO_NAME}'" >> /tmp/ci-data/dump.sh
+    echo "export REPO_NAME='${REPO_NAME}'" >> /tmp/ci-data.sh
 fi
 
 if [[ -n "${PULL_NUMBER}" ]]; then
-    echo "export PULL_NUMBER='${PULL_NUMBER}'" >> /tmp/ci-data/dump.sh
+    echo "export PULL_NUMBER='${PULL_NUMBER}'" >> /tmp/ci-data.sh
 fi
 
 if [[ -n "${PULL_BASE_REF}" ]]; then
-    echo "export PULL_BASE_REF='${PULL_BASE_REF}'" >> /tmp/ci-data/dump.sh
+    echo "export PULL_BASE_REF='${PULL_BASE_REF}'" >> /tmp/ci-data.sh
 fi
 
 if [[ -n "${CLONEREFS_OPTIONS}" ]]; then
-    echo "export CLONEREFS_OPTIONS='${CLONEREFS_OPTIONS}'" >> /tmp/ci-data/dump.sh
+    echo "export CLONEREFS_OPTIONS='${CLONEREFS_OPTIONS}'" >> /tmp/ci-data.sh
 fi
 
 if [[ -n "${JOB_SPEC}" ]]; then
-    echo "export JOB_SPEC='${JOB_SPEC}'" >> /tmp/ci-data/dump.sh
+    echo "export JOB_SPEC='${JOB_SPEC}'" >> /tmp/ci-data.sh
 fi
